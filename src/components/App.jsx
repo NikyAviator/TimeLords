6f7f7da9dd9cbb67086eae0b90b6582eb71d170f
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-01-18 14:37:22 +0100

    Organized code, created 'components' and 'image' folder. Links updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Header from './Header';
-import Favorites from './pages/Favorites';
-import Footer from './Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Home from '../pages/Home';
+import Favorites from '../pages/Favorites';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 function Favorites() {
   return (
     <div>
