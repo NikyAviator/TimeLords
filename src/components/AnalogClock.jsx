a300a005fafec618edb9a7a12df4c46fd828c73f
Punkfisen <axel.westerstrom@gmail.com>
2022-01-21 14:01:16 +0100

    Analog clock is now ticking at local time and changed the style abit

--- src/components/AnalogClock.jsx
+++ src/components/AnalogClock.jsx
@@ -1,39 +1,59 @@
-const AnalogClock = () => {
+import React, { Component } from "react";
 
-  setInterval(setClock, 1000)
-
-  function setClock() {
-    const currentDate = new Date()
-    const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = (secondsRatio + currentDate.getminutes()) / 60
-    const hoursRatio = (minutesRatio + currentDate.gethours()) / 12
+export default class AnalogClock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date()
+    };
   }
 
-  return (
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({
+        time: new Date()
+      });
+    }, 1000);
+  }
 
-    <div className="analogClock">
-      <div className="hourHand Hand" data-hour-hand></div>
-      <div className="minuteHand Hand" ></div>
-      <div className="secondHand Hand" ></div>
-      <div className="midCircle"></div>
+  componentWillMount() {
+    clearInterval(this.timerId);
+  }
 
+  render() {
+    return (
+      <div className="analogClock">
+        <div className="hourHand Hand"
+          style={{
+            transform: `rotateZ(${this.state.time.getHours() * 30}deg)`
+          }}></div>
+        <div className="minuteHand Hand"
+          style={{
+            transform: `rotateZ(${this.state.time.getMinutes() * 6}deg)`
+          }}></div>
 
-      <span className="clock1">1</span>
-      <span className="clock2">2</span>
-      <span className="clock3">3</span>
-      <span className="clock4">4</span>
-      <span className="clock5">5</span>
-      <span className="clock6">6</span>
-      <span className="clock7">7</span>
-      <span className="clock8">8</span>
-      <span className="clock9">9</span>
-      <span className="clock10">10</span>
-      <span className="clock11">11</span>
-      <span className="clock12">12</span>
+        <div className="secondHand Hand"
+          style={{
+            transform: `rotateZ(${this.state.time.getSeconds() * 6}deg)`
+          }}
+        >
+        </div>
+        <div className="midCircle"></div>
+        <span className="clock1">1</span>
+        <span className="clock2">2</span>
+        <span className="clock3">3</span>
+        <span className="clock4">4</span>
+        <span className="clock5">5</span>
+        <span className="clock6">6</span>
+        <span className="clock7">7</span>
+        <span className="clock8">8</span>
+        <span className="clock9">9</span>
+        <span className="clock10">10</span>
+        <span className="clock11">11</span>
+        <span className="clock12">12</span>
 
-    </div>
+      </div>
 
-  )
+    )
+  }
 }
-
-export default AnalogClock
