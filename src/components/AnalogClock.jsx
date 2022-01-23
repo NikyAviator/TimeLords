539aedc3ffee18d686f9e11dffd58ecb95bf7eae
Punkfisen <axel.westerstrom@gmail.com>
2022-01-23 13:17:10 +0100

    Tweeked and added a bit more style to the analog clock

--- src/components/AnalogClock.jsx
+++ src/components/AnalogClock.jsx
@@ -22,36 +22,38 @@ export default class AnalogClock extends Component {
 
   render() {
     return (
-      <div className="analogClock">
-        <div className="hourHand Hand"
-          style={{
-            transform: `rotateZ(${this.state.time.getHours() * 30}deg)`
-          }}></div>
-        <div className="minuteHand Hand"
-          style={{
-            transform: `rotateZ(${this.state.time.getMinutes() * 6}deg)`
-          }}></div>
+      <div className="analogClockBody">
+        <div className="analogClock">
+          <div className="hourHand Hand"
+            style={{
+              transform: `translateX(-50%) rotateZ(${this.state.time.getHours() * 30}deg) `
+            }}></div>
+          <div className="minuteHand Hand"
+            style={{
+              transform: `translateX(-50%) rotateZ(${this.state.time.getMinutes() * 6}deg) `
+            }}></div>
 
-        <div className="secondHand Hand"
-          style={{
-            transform: `rotateZ(${this.state.time.getSeconds() * 6}deg)`
-          }}
-        >
+          <div className="secondHand Hand"
+            style={{
+              transform: `translateX(-50%) rotateZ(${this.state.time.getSeconds() * 6}deg) `
+            }}
+          >
+          </div>
+          <div className="midCircle"></div>
+          <span className="clock1">1</span>
+          <span className="clock2">2</span>
+          <span className="clock3">3</span>
+          <span className="clock4">4</span>
+          <span className="clock5">5</span>
+          <span className="clock6">6</span>
+          <span className="clock7">7</span>
+          <span className="clock8">8</span>
+          <span className="clock9">9</span>
+          <span className="clock10">10</span>
+          <span className="clock11">11</span>
+          <span className="clock12">12</span>
+
         </div>
-        <div className="midCircle"></div>
-        <span className="clock1">1</span>
-        <span className="clock2">2</span>
-        <span className="clock3">3</span>
-        <span className="clock4">4</span>
-        <span className="clock5">5</span>
-        <span className="clock6">6</span>
-        <span className="clock7">7</span>
-        <span className="clock8">8</span>
-        <span className="clock9">9</span>
-        <span className="clock10">10</span>
-        <span className="clock11">11</span>
-        <span className="clock12">12</span>
-
       </div>
 
     )
