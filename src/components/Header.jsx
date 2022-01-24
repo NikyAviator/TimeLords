3394abaa20eb0d318b652136480e481c7347df00
Mayyadah Al-sallami <mayadasalih60@yahoo.com>
2022-01-24 14:53:46 +0100

    Ta bort

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <Navbar expand="lg" variant="light" bg="light">
       <Container>
-        <Navbar.Brand href="/">TimeLords 1 2 3</Navbar.Brand>
+        <Navbar.Brand href="/">TimeLords</Navbar.Brand>
       </Container>
     </Navbar>
   );
