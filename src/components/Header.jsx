5baf1b966e2a00cb4ff2f583a48b4e2dfffc805b
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-01-24 15:46:14 +0100

    Added off-canvas navbar. Button still invisible though.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,30 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {   Nav,Navbar,NavbarBrand, NavbarToggler, NavItem, NavLink, Collapse, Button } from 'reactstrap';
+import {   Nav,Navbar,Offcanvas, Container } from 'react-bootstrap';
 export default function Header() {
   return (
-    <div>
-  <Navbar
-    color="faded"
-    light>
-    <NavbarBrand
-      className="me-auto"
-      href="/"
+    <Navbar bg="light" expand={false}>
+  <Container fluid>
+    <Navbar.Brand href="#">TimeLords</Navbar.Brand>
+    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end"
     >
-      TimeLords
-    </NavbarBrand>
-    <NavbarToggler
-      className="me-2"
-      onClick={function noRefCheck(){}}
-    />
-    <Collapse navbar>
-      <Nav navbar>
-        <NavItem>
-          <NavLink href="/components/">
-            Components
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://github.com/reactstrap/reactstrap">
-            GitHub
-          </NavLink>
-        </NavItem>
-      </Nav>
-    </Collapse>
-  </Navbar>
-</div>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel">TimeLords</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/favorites">Favorites</Nav.Link>
+          <Nav.Link href="/biggest-cities">Biggest Cities</Nav.Link>
+        </Nav>
+        
+      </Offcanvas.Body>
+    </Navbar.Offcanvas>
+  </Container>
+</Navbar>
   );
 }
