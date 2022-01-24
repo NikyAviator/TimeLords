db8bbdebc4479edfc692130b886acf98329267f5
Sana <sanabarilad@gmail.com>
2022-01-24 19:11:11 +0100

    the clock is right now

--- src/components/DynamicTime.jsx
+++ src/components/DynamicTime.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 
 const DynamicTime = (props) => {
   let { timeZoneInfo } = props
 
   const [timeString, setTimeString] = useState('00:00:00')
 
-  useEffect(() => {
+  useEffect(function uppdateTime() {
     const interval = setInterval(() => {
       update()
     }, 1000);
     return () => clearInterval(interval)
-  }, [])
+  }, [update])
 
   function update() {
     let date = new Date()
     setTimeString(date.toLocaleTimeString('sv-SE', { timeZone: timeZoneInfo.timezone }))
   }
+
+
 
   return (
     <>{timeString}</>
