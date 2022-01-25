af87cdb5c9d769440d509057dc00b8277c9f8189
Sana <sanabarilad@gmail.com>
2022-01-25 13:36:06 +0100

    Added localStorage code in Detail.jsx and City.jsx

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,25 +1,35 @@
 
 import { Col, Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import store from './localStore';
 
 function City(props) {
   let { city } = props;
   let { city_name } = city;
 
+  store.city = store.city || [];
+  store.save();
+
+  function saveCity() {
+    store.city = city_name;
+    store.save();
+  }
+
+
 
   //Set each city and the name of it 
   return <>
 
-    <Col xs={6} md={4}>
+    <Col xs={5} md={4}>
       <Card>
         <>{ /* Link to Unsplash and generate random picture that matches the city */}</>
         <Card.Img variant="top"
           src={`https://source.unsplash.com/random/640x360/?${city_name.substring(city_name.indexOf("/") + 1).replace('_', ' ')}`} />
         <Card.Body>
           <Link to="/timezone-info"
-            state={{ city_name }}
+
           >
-            <Button variant="light" className="city">
+            <Button variant="light" className="city" onClick={saveCity}>
               {city_name.substring(city_name.indexOf("/") + 1).replace('_', ' ')}</Button></Link>
 
 
--- src/components/City_Info.jsx
+++ src/components/City_Info.jsx
@@ -1,33 +1,29 @@
 
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import DynamicTime from './DynamicTime'
+import store from './localStore';
 
 
 function CityInfo() {
-  const location = useLocation();
-  const { city_name } = location.state; //Get the passed variable
 
   const [timeZoneInfo, setTimeZoneInfo] = useState([]);
-
-
   useEffect(() => {
-    if (city_name != ' ') {
+    if (store.city != ' ') {
       async function getTimeZoneInfo() {
-        setTimeZoneInfo(await (await fetch(`http://worldtimeapi.org/api/timezone/${city_name}`)).json());
+        setTimeZoneInfo(await (await fetch(`http://worldtimeapi.org/api/timezone/${store.city}`)).json());
       }
 
       getTimeZoneInfo();
     }
   }, []);
 
-  let { timezone, datetime } = timeZoneInfo; //These information is from worldtimeapi.org
+  let { datetime } = timeZoneInfo; //These information is from worldtimeapi.org
   if (datetime != undefined) { datetime = datetime.slice(0, 10); }
 
   //the timezone information are ferched from API is stored in "timeZoneInfo"
   //we need to render this on the screen I guess
 
-  if (city_name != ' ' && timeZoneInfo != undefined) {
+  if (store.city != ' ' && timeZoneInfo != undefined) {
     return <>
       <DynamicTime {...{ timeZoneInfo }} />
     </>
