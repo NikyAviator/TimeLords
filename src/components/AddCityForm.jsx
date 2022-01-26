6880e790d55e87b8450ed5c0abbecc560321d869
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-01-26 12:15:10 +0100

    Shana, Axel & Nikolai: empty form - work in progress

--- src/components/AddCityForm.jsx
+++ src/components/AddCityForm.jsx
@@ -2,12 +2,17 @@ import { useState, useEffect } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
+import store from './localStore';
+
 
 const AddCityForm = () => {
   const [errors, setErrors] = useState([])
   const [options, setOptions] = useState([])
   const [city, setCity] = useState('')
   const [timeZone, setTimeZone] = useState('')
+  const [localStoreCity, setLocalStoreCity] = useState([])
+  // const [localStoreTimeZone, setLocalStoreTimeZone] = useState('')
+  const [input, setInput] = useState([])
 
   useEffect(async () => {
     //Get timezones from WorldTimeAPI
@@ -16,12 +21,12 @@ const AddCityForm = () => {
 
   function handleSubmit(e) {
     e.preventDefault();
-
     let errors = [];
 
     if (city === '') { errors.push('You need to enter a city name') }
     if (timeZone === "") { errors.push('You need to select a time zone') }
     setErrors(errors.length === 0 ? [] : errors)
+    e.target.reset();
   }
 
   function handleCityInputChange(e) {
@@ -32,8 +37,19 @@ const AddCityForm = () => {
   function handleSelectTimeZoneChange(e) {
     e.preventDefault()
     setTimeZone(e.target.value)
+    
   }
 
+  function saveCity() {
+    if (city != ' ') {
+      setLocalStoreCity(localStoreCity.push(city));
+      store.myCities = localStoreCity;
+      store.save();
+      console.log(store.myCities);
+    }
+  }
+  
+ 
   return (
     <>
       <Form onSubmit={handleSubmit}>
@@ -58,7 +74,7 @@ const AddCityForm = () => {
             Pick the correct time zone for your city
           </Form.Text>
         </Form.Group>
-        <Button variant="primary" type="sumbit" className="mb-3">Add City</Button>
+        <Button onClick={saveCity} variant="primary" type="sumbit" className="mb-3">Add City</Button>
       </Form>
       {errors.map((error, index) => {
         return <Alert key={index} className="mb-3" variant="danger">{error}</Alert>
