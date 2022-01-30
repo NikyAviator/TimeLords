2d6b778f725853bbea1886e6f18b07e3d3bd9644
Sana <sanabarilad@gmail.com>
2022-01-30 03:47:26 +0100

    fixed route for cities

--- src/components/City_Info.jsx
+++ src/components/City_Info.jsx
@@ -3,27 +3,27 @@ import { useEffect, useState } from 'react';
 import DynamicTime from './DynamicTime'
 import DetailCities from './DetailCities'
 import store from '../utilities/localStore';
+import { useParams } from 'react-router-dom';
 
 
 
-function CityInfo() {
+function CityInfo(props) {
 
   const [timeZoneInfo, setTimeZoneInfo] = useState([]);
   const [cities, setCities] = useState([])
-
 
   useEffect(async () => {
     setTimeZoneInfo(store.city);
     setCities(await (await fetch('/json/cities.json')).json())
   }, []);
 
-  let { datetime } = timeZoneInfo; //These information is from worldtimeapi.org
-  if (datetime != undefined) { datetime = datetime.slice(0, 10); }
+
 
   //the timezone information are ferched from API is stored in "timeZoneInfo"
   //we need to render this on the screen I guess
 
   return <>
+
     <DynamicTime {...{ timeZoneInfo }} />
     {cities.length > 0 && <DetailCities {...{ cities, setCities, timeZoneInfo, setTimeZoneInfo }} />}
   </>
--- src/pages/Clock.jsx
+++ src/pages/Clock.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header_2 from '../components/Header_2';
 import City_Info from '../components/City_Info';
+import store from '../utilities/localStore';
+import { useParams } from 'react-router-dom';
 function Clock() {
+
+  let { city } = useParams();
+  const [list, setList] = useState([]);
+
+  useEffect(async () => {
+    setList(await (await fetch('/json/cities.json')).json())
+  }, [city])
+
+  const result = list.filter(function (e) {
+    return e.city_name.includes(city);
+  });
+
+  for (const value of result) {
+    store.city = value.city_name;
+    store.save();
+  }
+
+  console.log(store.city)
   return (
     <>
       <>{ /*Header_2 är för de andra sidorna fö utom hemsidan*/}</>
