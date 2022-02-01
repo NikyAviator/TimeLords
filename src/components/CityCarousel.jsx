70db6e2c06750671440b46ce51f7d2955578c8fc
artur-berlikowski <a.berlikowski@gmail.com>
2022-02-01 19:13:26 +0100

    Created a base for a dynamic city carousel

---
+++ src/components/CityCarousel.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import { Container } from 'react-bootstrap'
+import localData from '../utilities/localStore'
+
+const CityCarousel = () => {
+  const [myCities, setMyCities] = useState([])
+  return (
+    <Container className="city-carousel">
+
+    </Container>
+  )
+}
+
+export default CityCarousel 
