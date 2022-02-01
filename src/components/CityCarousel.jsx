ac0b1d73edcc9774f94ea0ee4095aa1d79618b09
artur-berlikowski <a.berlikowski@gmail.com>
2022-02-01 21:19:22 +0100

    CityCarousels can now replace the DetailCities component and is written to be dynamic

--- src/components/CityCarousel.jsx
+++ src/components/CityCarousel.jsx
@@ -1,28 +1,101 @@
 import { useState, useEffect } from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap'
-import localData from '../utilities/localStore'
+import { useNavigate } from 'react-router-dom'
+import { Container, Row, Col, Button, Card } from 'react-bootstrap'
+import store from '../utilities/localStore'
 
 const CityCarousel = (props) => {
-  let { title } = props
+  let { cities, title } = props
+  let navigate = useNavigate()
 
-  const [myCities, setMyCities] = useState([])
+  const [start, setStart] = useState(0)
+  const [numberToRender, setNumberToRender] = useState(0)
 
-  const Thumbs = () => {
+  useEffect(() => {
+    function getNumberToRender() {
+      let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      let numberToRender = Math.floor(windowWidth / 200) - 1;
+      numberToRender = numberToRender === 0 ? 1 : numberToRender
+      setNumberToRender(numberToRender)
+    }
 
+    function handleResize() { getNumberToRender() }
+
+    window.addEventListener('resize', handleResize)
+
+    getNumberToRender();
+  }, [])
+
+  function handleClick(e) {
+    e.preventDefault()
+    store.city = cities[e.target.id].timezone
+    store.save()
+    let target = store.city.split('/')[1];
+    navigate('/' + target)
+  }
+
+  function back(e) {
+    e.preventDefault()
+    let index = start - 1 > 0 ? start - 1 : cities.length
+    console.log(index)
+    setStart(index)
+  }
+
+  function next(e) {
+    e.preventDefault()
+    let index = start + 1 < cities.length ? start + 1 : 0
+    console.log(index)
+    setStart(index)
+  }
+
+  const Thumbs = (props) => {
+    let { cities, numberToRender } = props
+
+    let display = []
+    if (cities.length > 0) {
+      let index = start < cities.length ? start : 0
+      for (let i = 0; i < numberToRender; i++) {
+        display.push(cities[index])
+        if (index + 1 < cities.length) {
+          index++
+        } else {
+          index = 0
+        }
+      }
+    }
+
+    return (
+      <>
+        {
+          display.length > 0 && display.map((city, index) => {
+            let imageLink = `https://source.unsplash.com/random/640x360/?${city.timezone.split('/')[1]}-downtown`
+            console.log(city.name)
+            return (
+              <Card key={index} id={cities.indexOf(city)} onClick={handleClick} style={{
+                backgroundImage: `url(${imageLink})`,
+                backgroundSize: 'auto 100%',
+                cursor: 'pointer'
+              }}>
+                <Button>{city.name}</Button>
+              </Card>
+            )
+          })
+        }
+      </>
+    )
   }
 
   return (
-    <Container className="city-carousel">
+    <Container className="city-carousel" fluid>
       <Row>
-        <Col>
+        <Col className="title p-2">
           {title}
         </Col>
       </Row>
       <Row>
-        <Col>
-          <Button></Button>
-          <Thumbs />
-          <Button></Button>
+        <Col className="thumbs-container p-3">
+          <Button id="back"><img src="/images/arrow.svg" className="arrow arrow-left" onClick={back} /></Button>
+          {cities.length > 0 && <Thumbs {...{ cities, numberToRender }} />}
+          <Button id="next"><img src="/images/arrow.svg" className="arrow arrow-right" onClick={next} /></Button>
         </Col>
       </Row>
     </Container>
--- src/components/City_Info.jsx
+++ src/components/City_Info.jsx
@@ -1,37 +1,43 @@
 
 import { useEffect, useState } from 'react';
-import DynamicTime from './DynamicTime'
-import DetailCities from './DetailCities'
-import store from '../utilities/localStore';
 import { useParams } from 'react-router-dom';
 import { Row, Container } from 'react-bootstrap';
-
-
+import DynamicTime from './DynamicTime'
+import DetailCities from './DetailCities'
+import CityCarousel from './CityCarousel'
+import store from '../utilities/localStore';
 
 function CityInfo(props) {
-
   const [timeZoneInfo, setTimeZoneInfo] = useState([]);
-  const [cities, setCities] = useState([])
+  const [citiesLargest, setCitiesLargest] = useState([])
 
   useEffect(async () => {
+    let citiesLargestJSON = await (await fetch('/json/cities_largest.json')).json()
+    //Empty array to store largest cities data
+    let citiesLargest = []
+    //Extract the city name and time zone from cities_larges.json and push them into the cities array as "objects"
+    citiesLargestJSON.map(city => citiesLargest.push({ name: getCityNameFromTimeZone(city.timezone), timezone: city.timezone }))
     setTimeZoneInfo(store.city);
-    setCities(await (await fetch('/json/cities.json')).json())
+    setCitiesLargest(citiesLargest)
   }, []);
 
+  function getCityNameFromTimeZone(timezone) { return timezone.split('/')[1].replace('_', ' ') }
 
-  return <>
-    <div className="clock-body" style={{
-      backgroundImage: `url(https://source.unsplash.com/random/?${store.city.substring(store.city.indexOf("/") + 1)}-downtown)`,
-      backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center"
-    }}>
-      <Container className="clock-page" >
-        <DynamicTime {...{ timeZoneInfo }} />
-      </Container>
-
-      <div>
-        {cities.length > 0 && <DetailCities {...{ cities, setCities, timeZoneInfo, setTimeZoneInfo }} />}</div>
-    </div>
-  </>
+  return (
+    <>
+      <div className="clock-body" style={{
+        backgroundImage: `url(https://source.unsplash.com/random/?${store.city.substring(store.city.indexOf("/") + 1)}-downtown)`,
+        backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center"
+      }}>
+        <Container className="clock-page" >
+          <DynamicTime {...{ timeZoneInfo }} />
+        </Container>
+        <CityCarousel {...{ cities: citiesLargest, title: 'Largest Cities' }} />
+        {/* <div>
+        {cities.length > 0 && <DetailCities {...{ cities, setCities, timeZoneInfo, setTimeZoneInfo }} />}</div>*/}
+      </div>
+    </>
+  )
 
 }
 export default CityInfo;
