ea35134ce2e25ca94bbd555c528b6ba07e701262
artur-berlikowski <a.berlikowski@gmail.com>
2022-02-01 19:31:20 +0100

    Created CityCarousel base structure

--- src/components/CityCarousel.jsx
+++ src/components/CityCarousel.jsx
@@ -1,12 +1,30 @@
 import { useState, useEffect } from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import localData from '../utilities/localStore'
 
-const CityCarousel = () => {
+const CityCarousel = (props) => {
+  let { title } = props
+
   const [myCities, setMyCities] = useState([])
+
+  const Thumbs = () => {
+
+  }
+
   return (
     <Container className="city-carousel">
-
+      <Row>
+        <Col>
+          {title}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button></Button>
+          <Thumbs />
+          <Button></Button>
+        </Col>
+      </Row>
     </Container>
   )
 }
